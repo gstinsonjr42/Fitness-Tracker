63a3171766cad51ed51215fe2105ad72d70016ed
Greg Stinson <gstinsonjr17@gmail.com>
2021-02-17 18:09:17 -0800

    api uses excersise id to track data in router.js

--- routes/router.js
+++ routes/router.js
@@ -26,7 +26,48 @@ router.post("/api/workouts",(req,res)=>
             {
                 res.json(err)
             })
-    })  
+    }) 
+router.get("/api/workouts/range", (req, res) => 
+    {
+        Workout.find({})
+        .then(dbWorkout =>
+            {
+                res.json(dbWorkout);
+            })
+        .catch(err => 
+            {
+                res.json(err);
+            });
+    });
+
+    router.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+          params.id,
+          { $push: { exercises: body } },
+      
+          { new: true, runValidators: true }
+        )
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      });
+
+/*router.put("/api/workouts/:id",(req,res)=>
+    {
+        Workout.findById({})
+        .then(dbWorkout =>
+            {
+                res.json(dbWorkout);
+            })
+        .catch(err => 
+            {
+                res.json(err);
+            });
+    })*/
+       
 router.get("/exercise", 
     function(req, res)
         {
@@ -38,4 +79,5 @@ router.get("/stats",
         {
             res.sendFile(path.join(__dirname, "../public/stats.html"));
         });
+
 module.exports = router;
