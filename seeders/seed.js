7a77dbdf4ed2c3d93a181c6508caa61b52364e5f
Greg Stinson <gstinsonjr17@gmail.com>
2021-02-17 18:27:01 -0800

    heroku push file updated

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("https://git.heroku.com/mysterious-forest-26465.git", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
